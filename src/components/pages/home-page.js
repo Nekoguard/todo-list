6d402044f237b2fad8f9cbaf706216bd7bda4741
Nekoguard <nekoguard@protonmail.com>
2022-02-12 14:45:26 +0300

    Al / implement home page

--- src/components/pages/home-page.js
+++ src/components/pages/home-page.js
@@ -2,7 +2,15 @@ import React from "react";
 
 const HomePage = () => {
   return (
-    <div>Home page</div>
+    <div className="home-page">
+      <div className="logo">
+        <span>TODO</span> List
+      </div>
+      <div className="description">
+        Hey! I prepared for you the useful app, which will help you not to forget about your plans!
+        Use the menu to navigate throw the app.
+      </div>
+    </div>
   );
 };
 
